8a240ca84d9828c1633a38655a2e8080c9960075
Jirung-E <jh10590326@gmail.com>
2022-08-08 18:42:02 +0900

    classes

--- __main__.py
+++ __main__.py
@@ -1,5 +1,15 @@
 from functions import *
+from classes import *
 
 sayHello()
 sayHelloTo("Jir")
 
+menu = Menu()
+print(f"{menu.getRandom()} 어떰?")
+cmenu = ChineseFood()
+print(f"{cmenu.getRandom()} 어떰?")
+
+dinner = StreetFood()
+print(f"{dinner.getRandom()} 먹고", end = ' ')
+dinner = Dessert()
+print(f"후식 {dinner.getRandom()}", end = '\n')
